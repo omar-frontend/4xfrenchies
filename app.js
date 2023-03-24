648d7a1e73952a0e2caa3a78a0ac0af4704f0e58
Omar Dakelbab <omar.frontend@gmail.com>
2023-03-24 20:20:12 +0300

    commit

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(
     cors({
         methods: ["GET", "POST", "DELETE", "PUT"],
         credentials: true,
-        // origin: ["http://localhost:5174", "http://localhost:5175","http://192.168.1.34:5173"]
+        //origin: ["http://localhost:5174", "http://localhost:5175", "http://192.168.1.34:5173"]
         origin: ["https://4xfrenchies.com", "https://admin.4xfrenchies.com"]
     })
 )
@@ -35,7 +35,7 @@ app.use("/api/faq", questionsRoutes)
 
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-app.use("/public", express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/public'));
 
 app.get("/", (req, res) => {
     res.send("Server working")
