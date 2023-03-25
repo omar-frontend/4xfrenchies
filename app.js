18f74825d0a9c1dfbdc606898fae0b17245b375f
Omar Dakelbab <omar.frontend@gmail.com>
2023-03-25 06:32:28 +0300

    commit

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ import petsRoutes from './routes/pets.js'
 import questionsRoutes from './routes/questions.js'
 import path from 'path';
 import { fileURLToPath } from 'url';
+import nodemailer from "nodemailer";
 
 const app = express();
 
@@ -36,6 +37,47 @@ app.use("/api/faq", questionsRoutes)
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 app.use(express.static(__dirname + '/public'));
+
+import smtpTransport from 'nodemailer-smtp-transport'; // this is important
+
+
+var transporter = nodemailer.createTransport(smtpTransport({
+    host: 'smtp.hostinger.com',
+    auth: {
+        user: 'info@4xfrenchies.com',
+        pass: 'iO^7G31$CnHd2wo'
+    },
+    secure: true
+}));
+
+transporter.verify((error) => {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log("Ready to Send");
+    }
+});
+app.post("/contact", (req, res) => {
+    
+    const name = req.body.firstname + " " + req.body.lastname;
+    const email = req.body.email;
+    const message = req.body.message;
+    const mail = {
+        from: name,
+        to: "info@4xfrenchies.com",
+        subject: "4xfrenchies Have a new email!",
+        html: `<p>Name: ${name}</p>
+             <p>Email: ${email}</p>
+             <p>Message: ${message}</p>`,
+    };
+    transporter.sendMail(mail, (error) => {
+        if (error) {
+            res.json({ status: "ERROR" });
+        } else {
+            res.json({ status: "Message Sent" });
+        }
+    });
+});
 
 app.get("/", (req, res) => {
     res.send("Server working")
